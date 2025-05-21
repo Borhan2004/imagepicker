d4111f37808a97e1bba1ff2cd21edebfaaf2f352
bdcalling <izaz3531ahmed@gmail.com>
2025-05-22 00:30:46 +0600

    imagepicker

---
+++ lib/homescreen.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_image_picker_app/imagepicker_controller.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  ImagepickerController controller = Get.put(ImagepickerController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text('GetX Imagepicker'),
+      ),
+      body: Obx(() {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: CircleAvatar(
+                radius: 60,
+                backgroundImage:
+                    controller.imagePath.isEmpty
+                        ? FileImage(File(controller.imagePath.toString()))
+                        : null,
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                controller.getImage();
+              },
+              child: Text('Pick Image'),
+            ),
+          ],
+        );
+      }),
+    );
+  }
+}
